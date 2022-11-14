80766a3335acdd5c7a5b432ad13387c828ed9086
Robin Bette <robinbette@hotmail.com>
2022-11-14 08:50:46 +0100

    * add separation of concerns by adding a logging class

---
+++ ArdalisRating/Logger/ConsoleLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace ArdalisRating.Logger
+{
+    public class ConsoleLogger
+    {
+        public void Log(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+    }
+}
